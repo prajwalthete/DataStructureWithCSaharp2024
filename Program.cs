40e73c9713190530c6ba4fe6e4e5949a3ca4af15
prajwal <prajwalthete98@gmail.com>
2024-02-13 18:30:32 +0530

    [PRAJWAL] Add : Queue implementation using array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,93 @@
-﻿Console.WriteLine("GETTING STARTED DS");
-Console.ReadKey();
+﻿public class LinearQueue
+{
+    private int[] arr;
+    private int rear, front;
+
+    public LinearQueue(int size)
+    {
+        arr = new int[size];
+        rear = -1;
+        front = -1;
+    }
+    public bool IsFull()
+    {
+        return rear == arr.Length - 1;
+    }
+    public bool IsEmpty()
+    {
+        return rear == front;
+    }
+    public void Push(int val)
+    {
+        if (IsFull())
+            throw new Exception("Queue is Full.");
+        rear++;
+        arr[rear] = val;
+    }
+    public void Pop()
+    {
+        if (IsEmpty())
+            throw new Exception("Queue is Empty.");
+        front++;
+    }
+    public int Peek()
+    {
+        if (IsEmpty())
+            throw new Exception("Queue is Empty.");
+        return arr[front + 1];
+    }
+}
+
+public class LinearQueueMain
+{
+    static void Main(string[] args)
+    {
+        LinearQueue q = new LinearQueue(6);
+        int choice, val;
+        // var sc = new System.IO.StreamReader(Console.OpenStandardInput());
+        do
+        {
+            Console.WriteLine("\n0. Exit\n1. Push\n2. Pop\n3. Peek\nEnter choice: ");
+            // choice = int.Parse(sc.ReadLine());
+            choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1: // push
+                    try
+                    {
+                        Console.Write("Enter value to push: ");
+                        val = int.Parse(Console.ReadLine());
+                        q.Push(val);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    break;
+                case 2: // pop
+                    try
+                    {
+                        val = q.Peek();
+                        q.Pop();
+                        Console.WriteLine("Popped: " + val);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    break;
+                case 3: // peek
+                    try
+                    {
+                        val = q.Peek();
+                        Console.WriteLine("Peek: " + val);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    break;
+            }
+        } while (choice != 0);
+    }
+}
