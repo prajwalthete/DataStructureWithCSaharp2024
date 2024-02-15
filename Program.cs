8c3575b6e5441f213ea5d24e9cb06d538d21d47a
prajwal <prajwalthete98@gmail.com>
2024-02-15 15:27:31 +0530

    [PRAJWAL] Add: Implemetation of dictionary

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿Console.WriteLine("GETTING STARTED DS");
-Console.ReadKey();
+﻿namespace dictionaryStudy;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> dictionary = new Dictionary<string, int>();
+        dictionary.Add("mangos", 11);
+        dictionary.Add("apple", 20);
+        dictionary.Add("bananas", 30);
+
+
+        // Checking if a key exists
+        bool containsKey = dictionary.ContainsKey("mangos");
+        Console.WriteLine("contains Mangos " + containsKey);
+
+        foreach (var item in dictionary)
+        {
+            Console.WriteLine($"key: {item.Key} ,value : {item.Value}");
+        }
+
+
+        // Accessing elements by key
+        Console.WriteLine("value of apple " + dictionary["apple"]);
+
+        // Getting the number of elements
+        int count = dictionary.Count;
+        Console.WriteLine(count);
+
+
+        // remove 
+        dictionary.Remove("mangos");
+        Console.WriteLine("after removing mangos");
+        foreach (var item in dictionary)
+        {
+            Console.WriteLine($"key: {item.Key} ,value : {item.Value}");
+        }
+
+        // Clearing the dictionary
+        dictionary.Clear();
+
+    }
+}
